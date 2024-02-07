eb8449409417635c0fa724ea4845ac2e3cc987fc
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-02-08 00:54:00 +0900

    240207시도

--- 받아온거/test5.js
+++ 받아온거/test5.js
@@ -1,5 +1,38 @@
-const all = document.querySelector("#navi");
+const wrap = document.querySelector(".pageWrap");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const next2 = document.querySelector(".next2");
+const prev2 = document.querySelector(".prev2");
 
-all.addEventListener("click", function () {
-  alert(`따로 자바만드니까 안뜨는데?`);
+next.addEventListener("click", function () {
+  alert(`첫번째 다음`);
+  wrap.animate([{ transform: "translateX(-300px)" }], {
+    duration: 1000,
+    easing: "ease-in",
+    fill: "both",
+  });
+});
+next2.addEventListener("click", function () {
+  alert(`두번째 다음`);
+  wrap.animate([{ transform: "translateX(-600px)" }], {
+    duration: 1000,
+    easing: "ease-in",
+    fill: "both",
+  });
+});
+prev.addEventListener("click", function () {
+  alert(`첫번째 이전`);
+  wrap.animate([{ transform: "translateX(0px)" }], {
+    duration: 1000,
+    easing: "ease-in",
+    fill: "both",
+  });
+});
+prev2.addEventListener("click", function () {
+  alert(`두번째 이전`);
+  wrap.animate([{ transform: "translateX(-300px)" }], {
+    duration: 1000,
+    easing: "ease-in",
+    fill: "both",
+  });
 });
